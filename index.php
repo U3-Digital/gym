18c7b6014b9263f10740dccbc466b36483ee2325
estudio <contacto@e-studio.com.mx>
2021-12-17 19:46:40 -0700

    Update index.php

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ require_once "admin/models/enlaces.php";
 <!doctype html>
 <html lang="en">
   <head>
-  	<title>Login 08</title>
+  	<title>Sistema para Gym</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
